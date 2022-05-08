5550368e62281db59ad90726dd962e3f429f85ff
MorganGui <morgan_guillemin@hotmail.com>
2022-05-08 16:58:55 +0200

    code cleaning

--- src/components/ShowProducts.vue
+++ src/components/ShowProducts.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="categories">
-    <div class="title">TOP <input type="number" id="txtNbShow" v-model="nbShow" v-on:change="onSearchChange()"> RATINGS : <input type="text" id="txtSearch" placeholder="rechercher" v-model="txtSearch" v-on:change="onSearchChange()"></div>
+    <div class="title">TOP 10 RATINGS :</div>
     <div class="products">
       <div v-for="p in topProducts" :key="p.id">
         <OneProduct :product="p" :cat="'all'"/>
@@ -25,10 +25,8 @@ import { ProductService } from '@/services/ProductService'
 import { Product } from '@/models/Product'
 import OneProduct from '@/components/OneProduct.vue'
 
-let nbShow = 10
 let products: Product[] = []
-let topProducts: Product[] = []
-let txtSearch = 'Mens'
+const topProducts: Product[] = []
 
 export default {
   name: 'ShowProducts',
@@ -43,43 +41,21 @@ export default {
     products = await ProductService.getProducts()
 
     /** Liste des categories */
-    /** Produit affiché en haut */
     const categories: string[] = []
-    const tmpProducts: Product[] = []
     products.forEach((p: Product) => {
       if (!categories.includes(p.category)) categories.push(p.category)
-      if (p.title.toLowerCase().includes(txtSearch.toLowerCase())) tmpProducts.push(p)
     })
 
-    for (let i = 0; i < nbShow; i++) {
-      if (tmpProducts.length > i) topProducts[i] = tmpProducts[i]
+    /** Produit affiché en haut */
+    for (let i = 0; i < 10; i++) {
+      if (products.length > i) topProducts[i] = products[i]
     }
 
     return {
       products,
       categories,
       topProducts,
-      mobile,
-      nbShow,
-      txtSearch
-    }
-  },
-  methods: {
-    onSearchChange () {
-      nbShow = +(document.getElementById('txtNbShow') as HTMLInputElement).value
-      txtSearch = (document.getElementById('txtSearch') as HTMLInputElement).value
-      topProducts = []
-      const tmpProducts: Product[] = []
-      products.forEach((p: Product) => {
-        if (p.title.toLowerCase().includes(txtSearch.toLowerCase())) tmpProducts.push(p)
-      })
-      for (let i = 0; i < nbShow; i++) {
-        if (tmpProducts.length > i - 1) topProducts[i] = tmpProducts[i]
-      }
-      console.log(topProducts)
-      return {
-        topProducts
-      }
+      mobile
     }
   }
 }
@@ -92,21 +68,6 @@ export default {
     margin: 25px 5% 5px 5%
     font-size: 20px
     font-weight: 700
-    input
-      background: none
-      border-style: none
-      color: #fff
-      font-size: 20px
-      font-weight: 700
-      text-align: center
-      transition: 200ms
-      &:hover, &:focus
-        margin-right: 0
-      &:nth-child(1)
-        width: 40px
-      &:nth-child(2)
-        width: 150px
-        margin-left: 5px
 
   .products
     display: flex
@@ -129,15 +90,6 @@ export default {
 @media (max-width: 900px)
   .categories .title
     font-size: 18px
-    input
-      font-size: 18px
-    input:nth-child(1)
-      width: 25px
-    input:nth-child(2)
-      width: calc(100vw - 200px)
-      max-width: 200px
-      margin-left: 0px
-      margin-right: -20px
 /** Pour PC */
 @media (min-width: 901px)
   .products
@@ -151,6 +103,4 @@ export default {
       border-radius: 12px
       &:hover
         background-color: #ddd
-  .categories .title input
-        margin-right: -15px
 </style>
